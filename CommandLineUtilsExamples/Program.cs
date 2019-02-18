a8ec63e26ddab0cbb58cc7f91b11ec903ab2814c
Adrian Banks <adrian.banks@red-gate.com>
2019-02-18 14:44:10 +0000

    Fix up the CommandLineUtils example

--- CommandLineUtilsExamples/Program.cs
+++ CommandLineUtilsExamples/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using Core;
 using McMaster.Extensions.CommandLineUtils;
 
@@ -16,8 +14,8 @@ namespace CommandLineUtilsExamples
             app.Command("add", addCommand =>
             {
                 addCommand.Description = "Adds two integers";
-                var number1 = addCommand.Argument<int>("int1", "The first integer to sum").IsRequired();
-                var number2 = addCommand.Argument<int>("int2", "The second integer to sum").IsRequired();
+                var number1 = addCommand.Option<int>("-i1|--int1", "The first integer to sum", CommandOptionType.SingleValue).IsRequired();
+                var number2 = addCommand.Option<int>("-i2|--int2", "The second integer to sum", CommandOptionType.SingleValue).IsRequired();
 
                 addCommand.OnExecute(() =>
                 {
@@ -30,12 +28,12 @@ namespace CommandLineUtilsExamples
             app.Command("concatenate", concatenateCommand =>
             {
                 concatenateCommand.Description = "Concatenates two strings";
-                var text1 = concatenateCommand.Argument("string1", "The first string").IsRequired();
-                var text2 = concatenateCommand.Argument("string2", "The second string").IsRequired();
+                var text1 = concatenateCommand.Option("-s1|--string1", "The first string", CommandOptionType.SingleValue).IsRequired();
+                var text2 = concatenateCommand.Option("-s2|--string2", "The second string", CommandOptionType.SingleValue).IsRequired();
 
                 concatenateCommand.OnExecute(() =>
                 {
-                    var result = StringOperations.Concat(text1.Value, text2.Value);
+                    var result = StringOperations.Concat(text1.Value(), text2.Value());
                     Console.WriteLine(result);
                     return 0;
                 });
@@ -44,7 +42,7 @@ namespace CommandLineUtilsExamples
             app.Command("double", doubleCommand =>
             {
                 doubleCommand.Description = "Doubles an integer";
-                var number = doubleCommand.Argument<int>("int", "The integer to double").IsRequired();
+                var number = doubleCommand.Option<int>("-i|--int", "The integer to double", CommandOptionType.SingleValue).IsRequired();
 
                 doubleCommand.OnExecute(() =>
                 {
@@ -57,11 +55,11 @@ namespace CommandLineUtilsExamples
             app.Command("duplicate", duplicateCommand =>
             {
                 duplicateCommand.Description = "Duplicates a string";
-                var text = duplicateCommand.Argument("string", "The string to duplicate").IsRequired();
+                var text = duplicateCommand.Option("-s|--string", "The string to duplicate", CommandOptionType.SingleValue).IsRequired();
 
                 duplicateCommand.OnExecute(() =>
                 {
-                    var result = StringOperations.Duplicate(text.Value);
+                    var result = StringOperations.Duplicate(text.Value());
                     Console.WriteLine(result);
                     return 0;
                 });
@@ -70,12 +68,11 @@ namespace CommandLineUtilsExamples
             app.Command("join", joinCommand =>
             {
                 joinCommand.Description = "Joins multiple strings";
-                // TODO: how to make this work?
-                var strings = joinCommand.Argument<IEnumerable<string>>("strings", "The strings to join").IsRequired();
+                var strings = joinCommand.Option("-s|--strings", "The strings to join", CommandOptionType.MultipleValue).IsRequired();
 
                 joinCommand.OnExecute(() =>
                 {
-                    var result = StringOperations.Join(strings.ParsedValue);
+                    var result = StringOperations.Join(strings.Values);
                     Console.WriteLine(result);
                     return 0;
                 });
@@ -84,11 +81,11 @@ namespace CommandLineUtilsExamples
             app.Command("reverse", reverseCommand =>
             {
                 reverseCommand.Description = "Reverses a string";
-                var text = reverseCommand.Argument("string", "The string to reverse").IsRequired();
+                var text = reverseCommand.Option("-s|--string", "The string to reverse", CommandOptionType.SingleValue).IsRequired();
 
                 reverseCommand.OnExecute(() =>
                 {
-                    var result = StringOperations.Reverse(text.Value);
+                    var result = StringOperations.Reverse(text.Value());
                     Console.WriteLine(result);
                     return 0;
                 });
